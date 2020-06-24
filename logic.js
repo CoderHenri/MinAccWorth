8796b1fdbad18664e77931f8b3b1260131ae587c
Henri0812 <p.metzner@gmx.at>
2020-06-24 15:47:32 +0200

    added a new category and price fetch works

--- logic.js
+++ logic.js
@@ -1,5 +1,83 @@
+var FloorPrices = [];
 
-async function GetLandData() {
+async function LoadFloorPrices() {
+    var url = "https://axieinfinity.com/graphql-server/graphql";
+
+    var LandDataArray = [];
+
+    //NormalAxiePrice
+    await  fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+            
+        body: JSON.stringify({
+            "operationName":"GetAxieBriefList","variables":{"from":0,"size":1,"sort":"PriceAsc","auctionType":"Sale","owner":null,"region":null,"criteria":{"parts":null,"bodyShapes":null,"classes":null,"stages":null,"numMystic":null,"pureness":null,"title":null,"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]}},
+            "query":"query GetAxieBriefList($auctionType: AxieAuctionType, $region: String, $criteria: AxieCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, region: $region, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  auction {\n    currentPrice\n    __typename\n  }\n  __typename\n}\n"})
+    })
+    .then(function(response) { 
+        return response.json(); 
+    })
+        
+    .then(function(data) {
+        LandDataArray.push(data);
+        console.log(data);
+        QuerySaver(data, "NormalAxiePrice");
+    });
+
+    //OriginAxiePrice
+    await  fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+            
+        body: JSON.stringify({
+            "operationName":"GetAxieBriefList","variables":{"from":0,"size":1,"sort":"PriceAsc","auctionType":"Sale","owner":null,"region":null,"criteria":{"parts":null,"bodyShapes":null,"classes":null,"stages":null,"numMystic":null,"pureness":null,"title":["Origin"],"breedable":null,"breedCount":null,"hp":[],"skill":[],"speed":[],"morale":[]}},
+            "query":"query GetAxieBriefList($auctionType: AxieAuctionType, $region: String, $criteria: AxieCriteria, $from: Int, $sort: SortBy, $size: Int, $owner: String) {\n  axies(auctionType: $auctionType, region: $region, criteria: $criteria, from: $from, sort: $sort, size: $size, owner: $owner) {\n    results {\n      ...AxieBrief\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment AxieBrief on Axie {\n  auction {\n    currentPrice\n    __typename\n  }\n  __typename\n}\n"})
+    })
+    .then(function(response) { 
+        return response.json(); 
+    })
+        
+    .then(function(data) {
+        LandDataArray.push(data);
+        console.log(data);
+        QuerySaver(data, "OriginAxiePrice");
+    });
+}
+
+function QuerySaver(data, Category) {
+    var Price = data.data.axies.results[0].auction.currentPrice;
+    Price = Price * Math.pow(10, -18);
+    Price = Math.round((Price + Number.EPSILON) * 10000) / 10000
+    console.log(Price);
+    document.getElementById(Category).innerHTML = Price + " ETH";
+}
+
+function SelectAddress() {
+    FirstOnclickCheck = 0; 
+  
+    var txt;
+    var PopUp = prompt("Please enter your ETH Address with your Axies:", "0x1E3934EA7E416F4E2BC5F7d55aE9783da0061475");
+    if (PopUp == null || PopUp == "") {
+        txt = "User cancelled the prompt!";
+    } else if (PopUp.startsWith("0x") && PopUp.length == 42) {
+        txt = PopUp;
+        document.getElementById("ETHAddress").innerHTML = txt;
+        GetAccountData(txt);
+    } else {
+        txt = "Please enter a real ETH Address";
+
+    }
+    document.getElementById("ETHAddress").innerHTML = txt;
+}
+
+async function GetAccountData() {
+    
     var url = "https://axieinfinity.com/graphql-server/graphql";
 
     var LandDataArray = [];
